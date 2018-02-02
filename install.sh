432f70a25fc13f84b5aae7fb5ad06dd938c230ec
Ubuntu <vidits@kth.se>
2018-02-02 13:52:00 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install g++
 sudo apt-get -y install libblas-* liblapack-* libfftw-*
 sudo apt-get -y install make cmake
 sudo apt-get -y install python2.7* 
-sudo apt-get -y install python-dev python-pip python-numpy
+sudo apt-get -y install python-dev python-pip python-tk python-numpy
 
 mkdir ~/tmp
 cd ~/tmp
@@ -24,7 +24,6 @@ cd boost_1_66_0/
 ./bootstrap.sh
 sudo ./b2 install --with-python
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 cd ~
 mkdir workspace
@@ -33,4 +32,6 @@ git clone https://github.com/vidits-kth/py-itpp.git
 cd py-itpp
 sudo make install
 
-export PYTHONPATH=$PYTHONPATH:~/workspace/py-itpp/lib/gcc
+cp ~/.bashrc ~/.bashrc.old
+echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib >> ~/.bashrc
+echo export PYTHONPATH=$PYTHONPATH:~/workspace/py-itpp/lib/gcc >> ~/.bashrc
