81316e6148825ec1a068bbec76eca38f1712f770
Vidit Saxena <vidits@kth.se>
2018-04-25 16:11:42 +0200

    Updated with python style and removed numpy dependence

--- examples/ber_over_awgn_channel.py
+++ examples/ber_over_awgn_channel.py
@@ -1,62 +1,71 @@
-# COPYRIGHT_NOTICE
+# -------------------------------------------------------------------------
+#
+# Copyright (C) 2016 CC0 1.0 Universal (CC0 1.0) 
+#
+# The person who associated a work with this deed has dedicated the work to
+# the public domain by waiving all of his or her rights to the work 
+# worldwide under copyright law, including all related and neighboring 
+# rights, to the extent allowed by law.
+#
+# You can copy, modify, distribute and perform the work, even for commercial 
+# purposes, all without asking permission.
+#
+# See the complete legal text at 
+# <https://creativecommons.org/publicdomain/zero/1.0/legalcode>
+#
+# -------------------------------------------------------------------------
+
+import py_itpp as pyp
 
 from matplotlib import pyplot as plt
-import numpy as np
-
-from py_itpp.base import bvec, vec, ivec
-from py_itpp.base import random
-
-from py_itpp.comm.modulator import modulator_1d
-from py_itpp.comm.error_counters import BERC
-from py_itpp.comm.hammcode import hamming_code
 
 def bit_error_ratio_uncoded_awgn(snr_db):
     '''Generate random bits'''
     nrof_bits = 3 * 100000
-    source_bits = random.randb(nrof_bits)
+    source_bits = pyp.random.randb(nrof_bits)
     
     '''Modulate the bits'''
-    modulator_ = modulator_1d()
-    constellation = vec('-1, 1')
-    symbols = ivec('0, 1')
+    modulator_ = pyp.modulator.modulator_1d()
+    constellation = pyp.vec.vec('-1, 1')
+    symbols = pyp.vec.ivec('0, 1')
     modulator_.set(constellation, symbols)
     tx_signal = modulator_.modulate_bits(source_bits)
     
     '''Add the effect of channel to the signal'''
     noise_variance = 1.0 / (pow(10, 0.1 * snr_db))
-    noise = random.randn(tx_signal.length())
-    noise *= np.sqrt(noise_variance)
+    noise = pyp.random.randn(tx_signal.length())
+    noise *= pyp.elem_math.sqrt(noise_variance)
     rx_signal = tx_signal + noise
     
     '''Demodulate the signal'''
     demodulated_bits = modulator_.demodulate_bits(rx_signal)
     
     '''Calculate the bit error ratio'''
-    return BERC.count_errors(source_bits, demodulated_bits, 0, 0, 0) / nrof_bits
+    return pyp.error_counters.BERC.count_errors(source_bits, demodulated_bits, 0, 0, 0) / nrof_bits
 
 def bit_error_ratio_hamming_awgn(snr_db):
     '''Hamming encoder and decoder instance'''
     k = 3 # (7,4) Hamming code
-    hamm = hamming_code(k)
+    hamm = pyp.hammcode.hamming_code(k)
     
     '''Generate random bits'''
     nrof_bits = k * 100000
-    source_bits = random.randb(nrof_bits)
+    source_bits = pyp.random.randb(nrof_bits)
     
     '''Encode the bits'''
     encoded_bits = hamm.encode(source_bits)
     
     '''Modulate the bits'''
-    modulator_ = modulator_1d()
-    constellation = vec('-1, 1')
-    symbols = ivec('0, 1')
+    modulator_ = pyp.modulator.modulator_1d()
+    constellation = pyp.vec.vec('-1, 1')
+    symbols = pyp.vec.ivec('0, 1')
     modulator_.set(constellation, symbols)
     tx_signal = modulator_.modulate_bits(encoded_bits)
     
     '''Add the effect of channel to the signal'''
     noise_variance = 1.0 / (pow(10, 0.1 * snr))
-    noise = random.randn(tx_signal.length())
-    noise *= np.sqrt(noise_variance)
+    noise = pyp.random.randn(tx_signal.length())
+    noise *= pyp.elem_math.sqrt(noise_variance)
     rx_signal = tx_signal + noise
     
     '''Demodulate the signal'''
@@ -66,7 +75,7 @@ def bit_error_ratio_hamming_awgn(snr_db):
     decoded_bits = hamm.decode(demodulated_bits) 
     
     '''Calculate the bit error ratio'''
-    return BERC.count_errors(source_bits, decoded_bits, 0, 0, 0) / nrof_bits
+    return pyp.error_counters.BERC.count_errors(source_bits, decoded_bits, 0, 0, 0) / nrof_bits
 
 if __name__ == '__main__':
     '''SNR range'''
--- examples/bler_over_awgn_channel.py
+++ examples/bler_over_awgn_channel.py
@@ -1,65 +1,73 @@
-# COPYRIGHT_NOTICE
+# -------------------------------------------------------------------------
+#
+# Copyright (C) 2016 CC0 1.0 Universal (CC0 1.0) 
+#
+# The person who associated a work with this deed has dedicated the work to
+# the public domain by waiving all of his or her rights to the work 
+# worldwide under copyright law, including all related and neighboring 
+# rights, to the extent allowed by law.
+#
+# You can copy, modify, distribute and perform the work, even for commercial 
+# purposes, all without asking permission.
+#
+# See the complete legal text at 
+# <https://creativecommons.org/publicdomain/zero/1.0/legalcode>
+#
+# -------------------------------------------------------------------------
+
+import py_itpp as pyp
 
 from matplotlib import pyplot as plt
-import numpy as np
-
-from py_itpp.base import bvec, vec, ivec
-from py_itpp.base import random
-
-from py_itpp.comm.modulator import modulator_1d, soft_method
-from py_itpp.comm.error_counters import BLERC
-from py_itpp.comm.hammcode import hamming_code
-from py_itpp.comm.turbo import turbo_codec
 
 def block_error_ratio_uncoded_awgn(snr_db, block_size):
     '''Generate random bits'''
     nrof_bits = 3 * 10000 * block_size
-    source_bits = random.randb(nrof_bits)
+    source_bits = pyp.random.randb(nrof_bits)
     
     '''Modulate the bits'''
-    modulator_ = modulator_1d()
-    constellation = vec('-1, 1')
-    symbols = ivec('0, 1')
+    modulator_ = pyp.modulator.modulator_1d()
+    constellation = pyp.vec.vec('-1, 1')
+    symbols = pyp.vec.ivec('0, 1')
     modulator_.set(constellation, symbols)
     tx_signal = modulator_.modulate_bits(source_bits)
     
     '''Add the effect of channel to the signal'''
     noise_variance = 1.0 / (pow(10, 0.1 * snr_db))
-    noise = random.randn(tx_signal.length())
-    noise *= np.sqrt(noise_variance)
+    noise = pyp.random.randn(tx_signal.length())
+    noise *= pyp.elem_math.sqrt(noise_variance)
     rx_signal = tx_signal + noise
     
     '''Demodulate the signal'''
     demodulated_bits = modulator_.demodulate_bits(rx_signal)
     
     '''Calculate the block error ratio'''
-    blerc = BLERC(block_size)
+    blerc = pyp.error_counters.BLERC(block_size)
     blerc.count(source_bits, demodulated_bits)
     return blerc.get_errorrate()
 
 def block_error_ratio_hamming_awgn(snr_db, block_size):
     '''Hamming encoder and decoder instance'''
     k = 3 # (7,4) Hamming code
-    hamm = hamming_code(k)
+    hamm = pyp.hammcode.hamming_code(k)
     
     '''Generate random bits'''
     nrof_bits = k * 10000 * block_size
-    source_bits = random.randb(nrof_bits)
+    source_bits = pyp.random.randb(nrof_bits)
     
     '''Encode the bits'''
     encoded_bits = hamm.encode(source_bits)
     
     '''Modulate the bits'''
-    modulator_ = modulator_1d()
-    constellation = vec('-1, 1')
-    symbols = ivec('0, 1')
+    modulator_ = pyp.modulator.modulator_1d()
+    constellation = pyp.vec.vec('-1, 1')
+    symbols = pyp.vec.ivec('0, 1')
     modulator_.set(constellation, symbols)
     tx_signal = modulator_.modulate_bits(encoded_bits)
     
     '''Add the effect of channel to the signal'''
     noise_variance = 1.0 / (pow(10, 0.1 * snr_db))
-    noise = random.randn(tx_signal.length())
-    noise *= np.sqrt(noise_variance)
+    noise = pyp.random.randn(tx_signal.length())
+    noise *= pyp.elem_math.sqrt(noise_variance)
     rx_signal = tx_signal + noise
     
     '''Demodulate the signal'''
@@ -69,54 +77,54 @@ def block_error_ratio_hamming_awgn(snr_db, block_size):
     decoded_bits = hamm.decode(demodulated_bits) 
     
     '''Calculate the block error ratio'''
-    blerc = BLERC(block_size)
+    blerc = pyp.error_counters.BLERC(block_size)
     blerc.count(source_bits, decoded_bits)
     return blerc.get_errorrate()
 
 def block_error_ratio_turbo_awgn(snr_db, interleaver_length):
   '''Create turbo_codec_instance'''
-  codec = turbo_codec()
+  codec = pyp.turbo.turbo_codec()
   
   '''Set codec parameters'''
-  gen = ivec(2)
+  gen = pyp.vec.ivec(2)
   gen[0] = 11
   gen[1] = 13
   constraint_length = 4
-  interleaver = ivec('0, 19, 14, 33, 28, 47, 42, 13, 8, 27, 22, 41, 36, 7, 2, 21, 16, 35, 30, 1, 44, 15, 10, 29, 24, 43, 38, 9, 4, 23, 18, 37, 32, 3, 46, 17, 12, 31, 26, 45, 40, 11, 6, 25, 20, 39, 34, 5')
-  codec.set_parameters(gen, gen, constraint_length, ivec())
+  interleaver = pyp.vec.ivec('0, 19, 14, 33, 28, 47, 42, 13, 8, 27, 22, 41, 36, 7, 2, 21, 16, 35, 30, 1, 44, 15, 10, 29, 24, 43, 38, 9, 4, 23, 18, 37, 32, 3, 46, 17, 12, 31, 26, 45, 40, 11, 6, 25, 20, 39, 34, 5')
+  codec.set_parameters(gen, gen, constraint_length, pyp.vec.ivec())
   codec.set_interleaver(interleaver)
   
   '''Generate random bits and encode them'''
   nrof_uncoded_bits = interleaver_length * 1000
-  uncoded_bits = random.randb(nrof_uncoded_bits)
-  encoded_bits = bvec()
+  uncoded_bits = pyp.random.randb(nrof_uncoded_bits)
+  encoded_bits = pyp.vec.bvec()
   codec.encode(uncoded_bits, encoded_bits)
   
   '''Modulate bits using BPSK'''
-  symbols = vec('1, -1')
-  bits2symbol = ivec('0, 1')
-  modulator_ = modulator_1d(symbols, bits2symbol)
+  symbols = pyp.vec.vec('1, -1')
+  bits2symbol = pyp.vec.ivec('0, 1')
+  modulator_ = pyp.modulator.modulator_1d(symbols, bits2symbol)
   tx_signal = modulator_.modulate_bits(encoded_bits)
   
   '''Add AWGN noise'''
   noise_variance = 1.0 / (pow(10, 0.1 * snr_db))
-  noise = random.randn(tx_signal.length())
-  noise *= np.sqrt(noise_variance)
+  noise = pyp.random.randn(tx_signal.length())
+  noise *= pyp.elem_math.sqrt(noise_variance)
   rx_signal = tx_signal + noise
   
   '''Demodulate received signal (soft bits, LOGMAP)'''
-  soft_bits = vec()
+  soft_bits = pyp.vec.vec()
   modulator_.demodulate_soft_bits(rx_signal, 
                                   noise_variance,
                                   soft_bits,
-                                  soft_method.LOGMAP)
+                                  pyp.modulator.soft_method.LOGMAP)
 
   '''Turbo decode the soft bits'''
-  decoded_bits = bvec()
-  codec.decode(soft_bits, decoded_bits, bvec())
+  decoded_bits = pyp.vec.bvec()
+  codec.decode(soft_bits, decoded_bits, pyp.vec.bvec())
     
   '''Count errors'''
-  blerc = BLERC(interleaver_length)
+  blerc = pyp.error_counters.BLERC(interleaver_length)
   blerc.count(decoded_bits, uncoded_bits)
   return blerc.get_errorrate()
 
