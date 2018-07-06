c3500a28bd77f5a92bf8922c05e78f0c1746e3c8
Ubuntu <vidits@kth.se>
2018-07-06 21:54:36 +0000

    Pre-requisites for python3

--- install_prerequisites_python3.sh
+++ install_prerequisites_python3.sh
@@ -3,17 +3,23 @@
 # only Python 2.* and try to install against that. If so, you will need to
 # manually go into ~/tmp/boost*/ and update the following in project-config.jam
 #    using python : 3.5 : /usr ;
+# Then run
+#    > sudo ./b2 install --with-python
 #
 # NOTE!
-# You will need to update src/makefile to point to Python3 libraries
+# You will need to update py_itpp/src/makefile to point to Python3 libraries
 #    -lboost_python3 -lboost_numpy3
-
+# and Python3 include path
+#    INCLUDE_PATH := $(INCLUDE_PATH) -I/usr/include/python3.5
 
 sudo apt-get update
 sudo apt-get -y install g++
 sudo apt-get -y install libblas-* liblapack-* libfftw-*
 sudo apt-get -y install make cmake
-#sudo apt-get -y install python2.7* 
+
+if command -v python3 &>/dev/null; then
+    sudo apt-get -y install python3 
+fi
 sudo apt-get -y install python3-dev python3-pip python3-tk python3-numpy
 
 
