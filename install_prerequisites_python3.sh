780ed4b4eefb7086649f246a24a1bd93447edb18
Ubuntu <vidits@kth.se>
2018-08-15 23:49:49 +0000

    Removed redundant installs and Updated to support Ubuntu 18

--- install_prerequisites_python3.sh
+++ install_prerequisites_python3.sh
@@ -4,13 +4,13 @@
 
 sudo apt-get update
 sudo apt-get -y install g++
-sudo apt-get -y install libblas-* liblapack-* libfftw-*
+sudo apt-get -y install libblas-dev liblapack-dev libfftw3-dev
 sudo apt-get -y install make cmake
 
-if command -v python3 &>/dev/null; then
-    sudo apt-get -y install python3 
-fi
-sudo apt-get -y install python3-dev python3-pip python3-tk python3-numpy
+#if command -v python3 &>/dev/null; then
+#    sudo apt-get -y install python3 
+#fi
+sudo apt-get -y install python3-dev python3-numpy
 
 
 # Install ITPP
